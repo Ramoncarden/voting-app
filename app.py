cebf6f44a5cbdc819f468190f7fdf6bd63661827
Ramon Cardenas <ramoncarden@gmail.com>
2020-08-13 23:27:50 -0500

    Added endpoins for obtaining government vote records and initial page styling design

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from flask import Flask, render_template, request, flash, redirect, session, g, abort
+import requests
+import json
+from flask import Flask, render_template, request, flash, redirect, session, g, abort, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
+from keys import key
 
-from forms import UserAddForm
+from forms import UserAddForm, LoginForm
 from models import db, connect_db, User
+
+BASE_URL = 'https://api.propublica.org/congress/v1/'
+MEMBERS_API_URL = 'https://api.propublica.org/congress/v1/116/senate/members.json'
+ALL_MEMBERS = 'https://api.propublica.org/congress/v1/116/senate/members.json'
+MEMBER_VOTE_POSITION_API = 'https://api.propublica.org/congress/v1/members/'
+BILL_API = 'https://api.propublica.org/congress/v1/bills/search.json?query={query}'
 
 CURR_USER_KEY = 'curr_user'
 
@@ -14,6 +23,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgres:///informed_voter_db')
 )
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
@@ -50,13 +60,6 @@ def do_logout():
         del session[CURR_USER_KEY]
 
 
-@app.route('/')
-def show_homepage():
-    """Return Homepage"""
-
-    return render_template('home-anon.html')
-
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """Handle user signup.
@@ -90,6 +93,115 @@ def signup():
     else:
         return render_template('users/signup.html', form=form)
 
-# @app.route('login', methods=['GET', 'POST'])
-# def login():
-#     """Handle user login"""
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Handle user login"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data, form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Welcome back {user.username}!", 'success')
+            return redirect('/')
+
+        flash('Invalid credentials', 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle user logout"""
+
+    do_logout()
+
+    flash("You have successfully logged out", 'success')
+    return redirect('/login')
+
+
+# *****************************************************
+# General User routes and search routes
+
+
+# *****************************************************
+# Homepage and error pages
+
+
+@app.route('/')
+def show_homepage():
+    """Return Homepage
+
+    - anon: no feed, history or likes
+    - logged in: show last 10 history searches and favorites
+    """
+    if g.user:
+        username = g.user.email
+        return render_template('home.html', username=username)
+
+    else:
+        return render_template('homepage.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route('/search/member')
+def do_search():
+    """Handle search data and send request to API"""
+
+    response = requests.get(f"{BASE_URL}/members/B001243.json",
+                            headers={'X-API-Key': key})
+    data = response.json()
+    first_name = data['results'][0]['first_name']
+    last_name = data['results'][0]['last_name']
+
+    return f"{first_name} {last_name}"
+
+
+@app.route('/search')
+def get_gov_official():
+    """Gather list of government officials"""
+
+    res = requests.get(ALL_MEMBERS,
+                       headers={'X-API-Key': key})
+
+    data = res.json()
+
+    id = [member['id'] for member in data['results'][0]['members']]
+    first_name = [member['first_name']
+                  for member in data['results'][0]['members']]
+    last_name = [member['last_name']
+                 for member in data['results'][0]['members']]
+
+    members = data['results'][0]['members']
+
+    return render_template('search/gov-officials.html', id=id, first_name=first_name, last_name=last_name, members=members)
+
+
+@app.route('/search/member/<member_id>')
+def get_member_info(member_id):
+    """Retrieve government official individual data on link click"""
+
+    res = requests.get(
+        f"{MEMBER_VOTE_POSITION_API}{member_id}/votes.json", headers={'X-API-Key': key})
+
+    data = res.json()
+    members_data = data['results'][0]['votes']
+
+    return render_template('search/officials_voting.html', members_data=members_data, member_id=member_id)
+
+
+@app.route('/search/bill')
+def get_bill_info():
+    """Retrieve all bill information"""
+
+    res = requests.get(BILL_API, headers={'X-API-key': key}, params="family")
+    data = res.json()
+
+    return data
--- forms.py
+++ forms.py
@@ -9,3 +9,10 @@ class UserAddForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
